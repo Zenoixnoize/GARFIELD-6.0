4b2915b874624970e542d3444f4f28bee6c0e410
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-03-25 17:38:31 +0530

    GARFIELD V6.0 BY NOIZE PROJECTS  🐼
    GARFIELD V6.0 BY NOIZE PROJECTS  🐼

---
+++ Aurora/index.js
@@ -0,0 +1,10 @@
+/*COPYRIGHT (C) 2022 CODED BY NOIZE */
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
