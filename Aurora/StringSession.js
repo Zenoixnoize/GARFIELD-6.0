7761c33b5ad060efc2ed2aa1a6acc0fe396316b7
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-03-25 17:35:13 +0530

    GARFIELD V6.0 BY NOIZE PROJECTS  🐼
    GARFIELD V6.0 BY NOIZE PROJECTS  🐼

---
+++ Aurora/StringSession.js
@@ -0,0 +1,29 @@
+/*COPYRIGHT (C) 2022 CODED BY NOIZE */
+
+const fs = require('fs');
+
+class StringSession {
+    constructor() {
+    }
+
+    deCrypt(string = undefined) {
+        if ('GARFIELD_SESSION' in process.env && string === undefined) {
+            string = process.env.STRING_SESSION;
+        } else if (string !== undefined) {
+            if (fs.existsSync(string)) {
+                string = fs.readFileSync(string, {encoding:'utf8', flag:'r'});
+            }
+        }
+        
+        var split = string.split(';;;');
+        if (split.length >= 2) {
+            return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
+        }
+    }
+
+    createStringSession(dict) {
+        return 'GARFIELD;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+    }
+}
+
+module.exports = StringSession;
