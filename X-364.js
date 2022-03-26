27f11eb4af7be85eed688f8c99dd13ace687cfe7
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-03-27 00:27:10 +0530

    GARFIELD V6.0 BY NOIZE PROJECTS 🐼
    GARFIELD V6.0 BY NOIZE PROJECTS 🐼

--- X-364.js
+++ X-364.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-// PLK Special Functions
+/*COPYRIGHT (C) 2022 CODED BY NOIZE */
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
