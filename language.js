4f462632daa80b0d37d62d092db4b21b0ea229ce
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-03-26 16:24:26 +0530

    GARFIELD V6.0 BY NOIZE PROJECTS  🐼
    GARFIELD V6.0 BY NOIZE PROJECTS  🐼

--- language.js
+++ language.js
@@ -2,12 +2,12 @@ const Config = require('./config');
 const fs = require('fs');
 const chalk = require('chalk');
 
-if (fs.existsSync('./language/' + Config.LANG + '.json')) {
+if (fs.existsSync('./language/EN.json')) {
     console.log(
         chalk.green.bold('Loading English Language...')
     );
 
-    var json = JSON.parse(fs.readFileSync('./language/' + Config.LANG + '.json'));
+    var json = JSON.parse(fs.readFileSync('./language/EN.json'));
 } else {
     console.log(
         chalk.red.bold('You entered an invalid language. English language was chosen.')
