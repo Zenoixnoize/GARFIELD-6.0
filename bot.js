07c74390f5a610b3d8913b317ea89c6e72faddd8
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-03-26 23:27:13 +0530

    GARFIELD V6.0 BY NOIZE PROJECTS 🐼
    GARFIELD V6.0 BY NOIZE PROJECTS 🐼

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ async function Aurora () {
     const conn = new WAConnection();
     const Session = new StringSession();
     
-    conn.browserDescription = ["X-nodes servers(SECURED)", "ZENOI", '6.0.0']
+    conn.browserDescription = ["X-nodes Server(SECURED)", "ZENOI", '6.0.0']
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
