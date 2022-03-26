9b4596bd70976487f1d9493f844ea33d1b99669c
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-03-27 00:21:15 +0530

    GARFIELD V6.0 BY NOIZE PROJECTS 🐼
    GARFIELD V6.0 BY NOIZE PROJECTS 🐼

---
+++ X-364.js
@@ -0,0 +1,16 @@
+/*COPYRIGHT (C) 2022 CODED BY NOIZE */
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
+ PHONE: process.env.NUMBER === undefined ? '+94711502119' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Ｚ Ξ ＮＯＩ' : process.env.DEPLOYER,    
+};
